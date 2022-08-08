3e938a7d55a146c1da66fd5e70a3c71211ecb90e
Francisco Lemus <franciscolemus072@gmail.com>
2022-08-08 05:47:39 -0500

    Add Employee.java

--- src/main/java/com/forte/prueba/models/Employee.java
+++ src/main/java/com/forte/prueba/models/Employee.java
@@ -1,25 +1,51 @@
 package com.forte.prueba.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
 @Table(name = "Employee")
-public class Employee {
+@Getter
+@Setter
+public class Employee extends CommonEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "EmployeeId",
+            nullable = false,
+            unique = true)
     private int employeeId;
 
+    @Column(name = "TaxIdNumber",
+            nullable = false,
+            unique = true,
+            length = 13)
     private String taxIdNumber;
 
+    @Column(name = "Name",
+            nullable = false,
+            length = 60)
     private String name;
 
-    private String lastname;
+    @Column(name = "LastName",
+            nullable = false,
+            length = 120)
+    private String lastName;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "BirthDate",
+            nullable = false)
     private Date birthDate;
 
+    @Column(name = "Email",
+            nullable = false)
     private String email;
 
+    @Column(name = "CellPhone",
+            nullable = false,
+            length = 20)
     private String cellPhone;
 }
