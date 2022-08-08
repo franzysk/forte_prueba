430db34a0e3f30dd3814119b8ab296eeb0472e1f
Francisco Lemus <franciscolemus072@gmail.com>
2022-08-08 05:36:16 -0500

    Add CommonEntity

--- src/main/java/com/forte/prueba/models/CommonEntity.java
+++ src/main/java/com/forte/prueba/models/CommonEntity.java
@@ -1,9 +1,23 @@
 package com.forte.prueba.models;
 
-import java.sql.Date;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
 public abstract class CommonEntity {
+    @Column(name = "IsActive",
+            nullable = false)
     private boolean isActive;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "DataCreated",
+            nullable = false)
     private Date dateCreated;
 }
